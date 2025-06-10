b12318d8ae1d060b0861179485ae35e231dc2172
Rich <barnaby.rich@etu.estia.fr>
2025-06-10 09:33:11 +0200

    fonction dimension

--- src/features.c
+++ src/features.c
@@ -17,3 +17,27 @@ void helloWorld() {
     printf("Hello World !");
 }
 
+
+/*
+fonction pour trouver la dimension de l'image
+Le paramètre c'est le source_path, pour qu'on puisse obtenir 
+les données de l'image et ici largeur (width) et hauteur (height)
+la fonction renvoie deux valeurs, largeur et hauteur
+*/ 
+
+
+void dimension(char *source_path) {
+    unsigned char *data = NULL;
+    int width, height, channels;
+
+    /*on vérifie sous nous avons bien des données pour évité tout beug*/
+    if (read_image_data(source_path, &data, &width, &height, &channels)) {
+        printf("dimension: %d, %d\n", width, height);
+    } else {
+        fprintf(stderr, "Erreur : impossible de lire l'image %s\n", source_path);
+    }
+
+    if (data != NULL) {
+        free(data);
+    }
+}
